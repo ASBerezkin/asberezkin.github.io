46a74408c455caed9fd1e69761b3c37d6025d584
ASBerezkin <tepmeh90694@yandex.ru>
2023-03-28 18:49:21 +0400

    Updates

---
+++ precache-manifest.eb620dd42ce69ad896df37bacdcf372f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0996f59e53b5c259149",
+    "url": "/static/css/main.df08d78f.chunk.css"
+  },
+  {
+    "revision": "f0996f59e53b5c259149",
+    "url": "/static/js/main.77cdb416.chunk.js"
+  },
+  {
+    "revision": "434968071acffc900843",
+    "url": "/static/js/runtime~main.858bc106.js"
+  },
+  {
+    "revision": "9964a7a7d6c2c54c5337",
+    "url": "/static/js/2.f6228eb1.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "60e2774256aef189acf3961dcef24512",
+    "url": "/static/media/photo.60e27742.png"
+  },
+  {
+    "revision": "172113a34f2f2f1b4e3d0cdc9427b4d2",
+    "url": "/static/media/moon.172113a3.svg"
+  },
+  {
+    "revision": "28dcf3f28f314b8f88434cb5cc22f7b1",
+    "url": "/static/media/sun.28dcf3f2.svg"
+  },
+  {
+    "revision": "b321c303ffad2f1e61c65cbff507ba08",
+    "url": "/static/media/contactArrow.b321c303.svg"
+  },
+  {
+    "revision": "4282855d661d306b135aca77aaf1acbf",
+    "url": "/index.html"
+  }
+];
