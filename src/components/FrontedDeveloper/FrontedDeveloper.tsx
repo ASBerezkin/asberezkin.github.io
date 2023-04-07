6c97d5346d63d8ae9180a060ac4d9e29b936ebc9
ASBerezkin <tepmeh90694@yandex.ru>
2023-04-07 21:19:33 +0400

    gradient on Frontend

--- src/components/FrontedDeveloper/FrontedDeveloper.tsx
+++ src/components/FrontedDeveloper/FrontedDeveloper.tsx
@@ -3,7 +3,9 @@ import './FrontedDeveloper.css'
 
 export const FrontedDeveloper: FC = () => {
   return <div className='frontedDeveloper__wrapper'>
-    <span className='frontedDeveloper text text__size-xl'>Fronted Developer</span>
+    <div className='frontedDeveloper__gradient'>
+      <span className='frontedDeveloper text text__size-xl'>Fronted Developer</span>
+    </div>
   </div>
 }
 
